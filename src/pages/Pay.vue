f50a685801afb7ecd7421a657df2ecd8b92dfc60
zhuxianbin <zhuxianbin@evcoming.com>
2018-02-24 15:43:33 +0800

    'test'

--- src/pages/Pay.vue
+++ src/pages/Pay.vue
@@ -39,8 +39,8 @@
                             </v-form-item>
 
                             <v-form-item :wrapper-col="{span:22,offset:2}" style="margin-top:24px">
-                                <a v-if='payStyle=="alipay"' class='ant-btn ant-btn-warning' target='_blank' :href='"http://aci-api.chaozhiedu.com/api/pay/alipay/"+payState.token'>转到支付</a>
-                                <a v-else class='ant-btn ant-btn-warning' @click="pay">转到支付</a>
+                                <a v-if='payStyle=="alipay"&&payState.token' class='ant-btn ant-btn-warning' target='_blank' :href='"http://aci-api.chaozhiedu.com/api/pay/alipay/"+payState.token'>转到支付</a>
+                                <a v-if="payStyle!='alipay'" class='ant-btn ant-btn-warning' @click="pay">转到支付</a>
                             </v-form-item>
                         </v-form>
                     </div>
