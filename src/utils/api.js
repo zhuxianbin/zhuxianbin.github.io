3df73c4988b7dac35abf0d19c7d75b407fc99ac1
zhuxianbin <zhuxianbin@evcoming.com>
2018-04-20 10:37:03 +0800

    接口地址修改

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import storage from "./storage";
 /**
  * 接口前缀
  * */
-var contentPath = "" || "http://aci-api.chaozhiedu.com";
+var contentPath = "http://101.201.222.8:8081" || "http://aci-api.chaozhiedu.com";
 
 var conmonAjax = function(url, param, type) {
     param = param || {};
