c3f190edde38da4f60a82bfa2358282ee64f9cca
zhuxianbin <zhuxian123@qq.com>
2018-08-12 22:49:52 +0800

    修改https

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ import {
 
 export const baseURL =
     // "http://101.201.222.8:8081" ||
-    "http://aci-api.chaozhiedu.com";
+    "https://aci-api.chaozhiedu.com";
 
 export const Token = function () {
     return getToken()
