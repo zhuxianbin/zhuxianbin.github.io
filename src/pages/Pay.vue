34fd070e4ee0b10bbbbc8c182bf0ac44f6916557
zhuxianbin <zhuxian123@qq.com>
2018-08-13 22:04:31 +0800

    123321

--- src/pages/Pay.vue
+++ src/pages/Pay.vue
@@ -1,118 +1,118 @@
 <template>
-    <el-container>
-        <el-header>
-            <layoutHeader></layoutHeader>
-        </el-header>
-        <el-container>
-            <el-main>
-                <div class="male-main" v-if="!!payData">
-                    <div class="ant-layout">
-                        <div class="ant-layout ant-layout-has-sider">
-                            <div class="ant-layout" style="padding: 20px 0;background: #fff;">
-                                <div style="width: 1200px;margin: 0 auto;background: #fff;">
-                                    <div style="margin-bottom: 10px;font-size: 16px;color: #56509F;">超职商城-确认支付</div>
-                                    <div class="margin-top-20 color-6" style="padding: 0 20px;background: #F6F6F6;border: 1px solid #EBEBEB;line-height: 50px;">
-                                        请确认您的订单信息，并按提示完成支付
-                                    </div>
+		<el-container>
+				<el-header>
+						<layoutHeader></layoutHeader>
+				</el-header>
+				<el-container>
+						<el-main>
+								<div class="male-main" v-if="!!payData">
+										<div class="ant-layout">
+												<div class="ant-layout ant-layout-has-sider">
+														<div class="ant-layout" style="padding: 20px 0;background: #fff;">
+																<div style="width: 1200px;margin: 0 auto;background: #fff;">
+																		<div style="margin-bottom: 10px;font-size: 16px;color: #56509F;">超职商城-确认支付</div>
+																		<div class="margin-top-20 color-6" style="padding: 0 20px;background: #F6F6F6;border: 1px solid #EBEBEB;line-height: 50px;">
+																				请确认您的订单信息，并按提示完成支付
+																		</div>
 
-                                    <el-form direction="horizontal" label-width="120px" class="margin-top-20">
-                                        <el-form-item label="订单内容">
-                                            <span class="ant-form-text" style="font-size: 18px;">{{payData.product_name}}</span>
-                                        </el-form-item>
-                                        <el-form-item label="订单价格" >
-                                            <div class="inline-block" style="font-size: 28px;margin-right: 20px;">
-                                                <span class="color-warn">{{(payData.price/100).toFixed(2)}}</span>元</div>
-                                            <el-button type="default" html-type="button" @click="refreshPrice()">刷新价格</el-button>
-                                        </el-form-item>
-                                        <el-form-item label="支付方式">
-                                            <el-radio-group v-model="payType" @change="getOrderPay">
-                                                <el-radio label="wechat">微信支付</el-radio>
-                                                <el-radio label="alipay">支付宝</el-radio>
-                                            </el-radio-group>
-                                        </el-form-item>
-                                        <el-form-item v-if='payType=="wechat"' label="扫码支付" >
-                                            <div style='width:200px;height:200px;padding:10px;border:1px solid #eee;'>
-                                                <img :src='qrcode' style='width:100%;' />
-                                            </div>
-                                        </el-form-item>
-                                        <el-form-item v-if='payType=="alipay"' label="">
-                                            <el-button type="primary" @click='jumpAliPage()'>去支付宝支付</el-button>
-                                        </el-form-item>
-                                        <el-form-item label="" v-if="payData.agreement">
-                                            <span>购买及表示您同意《
-                                                <a class="link" @click="dolookAgreement" href='javascript:;'>{{payData.product_name}}·协议</a>》</span>
-                                            <!-- <span v-if='payData.id==20'>购买及表示您同意《
-                                                <a class="link" href='http://news.chaozhiedu.com/wj/jinpailubo.docx'>【超职教育】18年＜金牌录播＞·协议</a>》</span>
-                                            <span v-if='payData.id==10'>购买及表示您同意《
-                                                <a class="link" href='http://news.chaozhiedu.com/wj/vipshicao.docx'>【超职教育】18年＜VIP保障实操班＞·协议</a>》</span>
-                                            <span v-if='payData.id==9'>购买及表示您同意《
-                                                <a class="link" href='http://news.chaozhiedu.com/wj/gaoxiaoquzheng.docx'>【超职教育】18年＜高效私教取证班＞·协议</a>》</span>
-                                            <span v-if='payData.id==8'>购买及表示您同意《
-                                                <a class="link" href='http://news.chaozhiedu.com/wj/lingjichutezhao.docx'>【超职教育】18年＜零基础特招班＞· 协议</a>》</span> -->
-                                        </el-form-item>
-                                        <el-form-item label="支持支付类型">
-                                            <div style='width:320px;overflow:hidden;'>
-                                                <img src="/static/images/20180316111851.png">
-                                            </div>
-                                        </el-form-item>
-                                    </el-form>
-                                    <div v-html='alipay'></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <el-dialog title="支付提醒" width="800" :show-close='false' :close-on-press-escape='false' :close-on-click-modal='false' :visible.sync="dialogSuccess">
-                        <el-row>
-                          <el-col :span="6">
-                            <div class="text-center" style='padding-top:20px;'>
-                                <i class="iconfont icon-chenggong" style="font-size: 70px;color: #7ED321;"></i>
-                            </div>
-                          </el-col>
-                          <el-col :span="18">
-                               <div style="font-size: 24px;">
-                                    恭喜您，支付成功
-                                </div>
-                                <div class="margin-top-20" style="font-size: 14px;">
-                                    订单信息：{{payData.product_name}}
-                                </div>
-                                <div class="margin-top-20" style="font-size: 14px;color: #FF4000;">
-                                    <el-button @click="$router.push('/male')" type="primary">继续挑选课程</el-button>
-                                    <el-button @click="$router.push('/courselist')" type="default" style="margin-left: 10px;">进入我的课程</el-button>
-                                </div>
-                                <div class="margin-top-20" style="font-size: 18px;">支付遇到问题：请联系010-51657777</div>
-                          </el-col>
-                        </el-row>
-                    </el-dialog>
+																		<el-form direction="horizontal" label-width="120px" class="margin-top-20">
+																				<el-form-item label="订单内容">
+																						<span class="ant-form-text" style="font-size: 18px;">{{payData.product_name}}</span>
+																				</el-form-item>
+																				<el-form-item label="订单价格" >
+																						<div class="inline-block" style="font-size: 28px;margin-right: 20px;">
+																								<span class="color-warn">{{(payData.price/100).toFixed(2)}}</span>元</div>
+																						<el-button type="default" html-type="button" @click="refreshPrice()">刷新价格</el-button>
+																				</el-form-item>
+																				<el-form-item label="支付方式">
+																						<el-radio-group v-model="payType" @change="getOrderPay">
+																								<el-radio label="wechat">微信支付</el-radio>
+																								<el-radio label="alipay">支付宝</el-radio>
+																						</el-radio-group>
+																				</el-form-item>
+																				<el-form-item v-if='payType=="wechat"' label="扫码支付" >
+																						<div style='width:200px;height:200px;padding:10px;border:1px solid #eee;'>
+																								<img :src='qrcode' style='width:100%;' />
+																						</div>
+																				</el-form-item>
+																				<el-form-item v-if='payType=="alipay"' label="">
+																						<el-button type="primary" @click='jumpAliPage()'>去支付宝支付</el-button>
+																				</el-form-item>
+																				<el-form-item label="" v-if="payData.agreement">
+																						<span>购买及表示您同意《
+																								<a class="link" @click="dolookAgreement" href='javascript:;'>{{payData.product_name}}·协议</a>》</span>
+																						<!-- <span v-if='payData.id==20'>购买及表示您同意《
+																								<a class="link" href='http://news.chaozhiedu.com/wj/jinpailubo.docx'>【超职教育】18年＜金牌录播＞·协议</a>》</span>
+																						<span v-if='payData.id==10'>购买及表示您同意《
+																								<a class="link" href='http://news.chaozhiedu.com/wj/vipshicao.docx'>【超职教育】18年＜VIP保障实操班＞·协议</a>》</span>
+																						<span v-if='payData.id==9'>购买及表示您同意《
+																								<a class="link" href='http://news.chaozhiedu.com/wj/gaoxiaoquzheng.docx'>【超职教育】18年＜高效私教取证班＞·协议</a>》</span>
+																						<span v-if='payData.id==8'>购买及表示您同意《
+																								<a class="link" href='http://news.chaozhiedu.com/wj/lingjichutezhao.docx'>【超职教育】18年＜零基础特招班＞· 协议</a>》</span> -->
+																				</el-form-item>
+																				<el-form-item label="支持支付类型">
+																						<div style='width:320px;overflow:hidden;'>
+																								<img src="/static/images/20180316111851.png">
+																						</div>
+																				</el-form-item>
+																		</el-form>
+																		<div v-html='alipay'></div>
+																</div>
+														</div>
+												</div>
+										</div>
+										<el-dialog title="支付提醒" width="800" :show-close='false' :close-on-press-escape='false' :close-on-click-modal='false' :visible.sync="dialogSuccess">
+												<el-row>
+													<el-col :span="6">
+														<div class="text-center" style='padding-top:20px;'>
+																<i class="iconfont icon-chenggong" style="font-size: 70px;color: #7ED321;"></i>
+														</div>
+													</el-col>
+													<el-col :span="18">
+															 <div style="font-size: 24px;">
+																		恭喜您，支付成功
+																</div>
+																<div class="margin-top-20" style="font-size: 14px;">
+																		订单信息：{{payData.product_name}}
+																</div>
+																<div class="margin-top-20" style="font-size: 14px;color: #FF4000;">
+																		<el-button @click="$router.push('/male')" type="primary">继续挑选课程</el-button>
+																		<el-button @click="$router.push('/courselist')" type="default" style="margin-left: 10px;">进入我的课程</el-button>
+																</div>
+																<div class="margin-top-20" style="font-size: 18px;">支付遇到问题：请联系010-51657777</div>
+													</el-col>
+												</el-row>
+										</el-dialog>
 
-                    <el-dialog title="支付提醒" width="800" :visible.sync="dialogFail">
-                        <el-row>
-                            <el-col :span="6">
-                                <div class="text-center" style='padding-top:20px;'>
-                                    <i class="iconfont icon-warning" style="font-size: 70px;color: #FF4000;"></i>
-                                </div>
-                            </el-col>
-                            <el-col :span="18">
-                                <div style="font-size: 24px;">
-                                    支付失败，请点击重新支付
-                                </div>
-                                <div class="margin-top-20" style="font-size: 14px;">
-                                    订单信息：{{payData.product_name}}
-                                </div>
-                                <div class="margin-top-20">
-                                    <el-button type="error" @click='dialogFail = false '>重新支付</el-button>
-                                    <el-button type="default" @click="$router.push('/male')" style="margin-left: 10px;">返回商城</el-button>
-                                </div>
-                                <div class="margin-top-20" style="font-size: 18px;">支付遇到问题：请联系010-51657777</div>
-                            </el-col>
-                        </el-row>
-                    </el-dialog>
-                    <el-dialog title="协议说明" top="20px" width="800px" :visible.sync="dialog.show">
-                        <div v-html="payData.agreement"></div>
-                    </el-dialog>
-                </div>
-            </el-main>
-        </el-container>
-    </el-container>
+										<el-dialog title="支付提醒" width="800" :visible.sync="dialogFail">
+												<el-row>
+														<el-col :span="6">
+																<div class="text-center" style='padding-top:20px;'>
+																		<i class="iconfont icon-warning" style="font-size: 70px;color: #FF4000;"></i>
+																</div>
+														</el-col>
+														<el-col :span="18">
+																<div style="font-size: 24px;">
+																		支付失败，请点击重新支付
+																</div>
+																<div class="margin-top-20" style="font-size: 14px;">
+																		订单信息：{{payData.product_name}}
+																</div>
+																<div class="margin-top-20">
+																		<el-button type="error" @click='dialogFail = false '>重新支付</el-button>
+																		<el-button type="default" @click="$router.push('/male')" style="margin-left: 10px;">返回商城</el-button>
+																</div>
+																<div class="margin-top-20" style="font-size: 18px;">支付遇到问题：请联系010-51657777</div>
+														</el-col>
+												</el-row>
+										</el-dialog>
+										<el-dialog title="协议说明" top="20px" width="800px" :visible.sync="dialog.show">
+												<div v-html="payData.agreement"></div>
+										</el-dialog>
+								</div>
+						</el-main>
+				</el-container>
+		</el-container>
 </template>
 
 <script>
